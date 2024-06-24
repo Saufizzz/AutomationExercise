15a59db2a5913380134aea5cce3826402f04434d
Saufizzz <muhdsaufi2110@gmail.com>
2024-06-24 11:00:42 +0800

    bug fix

--- Utilities/api_helper.py
+++ Utilities/api_helper.py
@@ -2,7 +2,7 @@
 import requests
 
 def create_user(name, email, password):
-    url = "https://example.com/api/register"
+    url = "https://automationexercise.com/"
     payload = {
         "name": name,
         "email": email,
@@ -12,11 +12,11 @@ def create_user(name, email, password):
     return response
 
 def get_user(email):
-    url = f"https://example.com/api/users?email={email}"
+    url = f"https://automationexercise.com/api/users?email={email}"
     response = requests.get(url)
     return response
 
 def delete_user(user_id):
-    url = f"https://example.com/api/users/{user_id}"
+    url = f"https://automationexercise.com/api/users/{user_id}"
     response = requests.delete(url)
     return response
